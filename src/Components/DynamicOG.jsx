8a370ab7ba4cb4bc7cd389e825426ec435942ad8
Damilarr <eaziaed@gmail.com>
2023-10-29 14:48:08 +0100

    og:title

--- src/Components/DynamicOG.jsx
+++ src/Components/DynamicOG.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 const DynamicOG = ({
   url = "https://whispme.vercel.app",
-  name = "WHISPER",
+  name = "WHISPER.",
 }) => {
   return (
     <Helmet>
-      <meta property="og:site_name" content={name} />
+      <meta property="og:title" content={name} />
       <meta property="og:url" content={url} />
     </Helmet>
   );
