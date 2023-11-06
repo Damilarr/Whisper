3814b81025889aecff02e3169c193299ecffa130
Emmanuel Adeyemo <99141207+Damilarr@users.noreply.github.com>
2023-11-06 15:11:40 +0100

    Update MessageView.jsx

--- src/Pages/MessageView.jsx
+++ src/Pages/MessageView.jsx
@@ -90,6 +90,7 @@ const MessageView = () => {
             <ReactPaginate
               className="py-5 justify-center items-center space-x-2 paginationn w-3/4 mx-auto flex"
               onPageChange={paginate}
+              pageRangeDisplayed={6}
               pageCount={Math.ceil(user.messages.length / messagesPerPage)}
               previousLabel={"Prev"}
               nextLabel={"Next"}
